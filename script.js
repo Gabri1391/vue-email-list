420d048a2b833ed10c5e1feeb9e0f45e050e8553
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-20 17:22:49 +0200

    push

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 Vue.config.devtools = true;
+
+const root = new Vue ({
+    el: '#root',
+    data:{
+        items: 10,
+        mail:'',
+        randomMailsArray: []
+    },
+    methods: {
+
+    },
+    created(){
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
+            then((res) => {
+                const mail = res.data.response
+                this.randomMailsArray.push(mail)
+               
+                console.log(randomMailsArray)
+            });
+        };
+    }
+
+});
